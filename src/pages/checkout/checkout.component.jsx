7699b4ce128ef187dfe2d7b5917bae3cc9ba8004
elenagolovko <airdragon220@gmail.com>
2020-03-09 12:24:01 +0100

    adding stripe intergation, updating favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,8 @@ import {
   selectCartTotal
 } from "../../_store/cart/cart.selectors";
 
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import "./checkout.styles.scss";
@@ -36,6 +38,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card fro payment*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
